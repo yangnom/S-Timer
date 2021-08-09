f189bed5e641a7a11fc08d1716af77fede7ad5af
Yangnom <ynom@pm.me>
2021-08-08 23:12:36 -0700

    versions

--- Tests/StimerTests/StimerTests.swift
+++ Tests/StimerTests/StimerTests.swift
@@ -2,10 +2,10 @@
     @testable import Stimer
 
     final class StimerTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(Stimer().text, "Hello, World!")
-        }
+//        func testExample() {
+//            // This is an example of a functional test case.
+//            // Use XCTAssert and related functions to verify your tests produce the correct
+//            // results.
+//            XCTAssertEqual(Stimer().text, "Hello, World!")
+//        }
     }
